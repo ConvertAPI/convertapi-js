a25e97b1bab05cea12bfc9550b362d69f7eeb4f6
Jonas Jasas <jonas.jasas@gmail.com>
2022-08-03 09:00:13 +0300

    urlencode fix 1.1.6

--- src/file-param.ts
+++ src/file-param.ts
@@ -17,7 +17,7 @@ export class FileValue {
 export default class FileParam implements IParam {
     /**
      * Single file conversion parameter class.
-     * 
+     *
      * @param name - parameter name.
      * @param file - value.
      * @param host - ConvertApi server domain.
@@ -30,12 +30,12 @@ export default class FileParam implements IParam {
 
     public value(): Promise<string> {
         if (this.file instanceof FileValue) {
-            return Promise.resolve(this.file.fileId)                
+            return Promise.resolve(this.file.fileId)
         } else {
             let uploadUrl = `https://${this.host}/upload?`
             let response = this.file instanceof URL
-                ? fetch(`${uploadUrl}url=${this.file.href}`, <RequestInit>{ method: 'POST' })
-                : fetch(`${uploadUrl}filename=${this.file.name}`, <RequestInit>{ method: 'POST', body: this.file })
+                ? fetch(`${uploadUrl}url=${encodeURIComponent(this.file.href)}`, <RequestInit>{ method: 'POST' })
+                : fetch(`${uploadUrl}filename=${encodeURIComponent(this.file.name)}`, <RequestInit>{ method: 'POST', body: this.file })
             return response
                 .then(r => r.ok ? r.json() : Promise.reject(<IResultErrorDto>{Code: 5007, Message: `Unable to upload the file`}))
                 .then(obj => obj.FileId)
