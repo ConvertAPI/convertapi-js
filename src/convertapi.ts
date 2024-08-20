c95f6b0055aea230fc08bd134d66382f40638257
Jonas Jasas <jonas.jasas@gmail.com>
2024-08-20 11:40:55 +0300

    Bearer Authentication

--- src/convertapi.ts
+++ src/convertapi.ts
@@ -2,11 +2,6 @@ import Params, {IParamInit} from "./params.js";
 import {IParams} from "./param.js";
 import Result, {IResultErrorDto} from "./result.js";
 
-interface TokenCredentials {
-    apiKey:  string
-    token: string
-}
-
 export default class ConvertApi {
 
     /**
@@ -16,12 +11,12 @@ export default class ConvertApi {
      * @param host - ConvertApi server domain name
      * @returns - New ConvertApi object
      */
-    static auth(credentials: string | TokenCredentials, host?: string): ConvertApi {
+    static auth(credentials: string, host?: string): ConvertApi {
         return new ConvertApi(credentials, host)
     }
 
     constructor(
-        public readonly credentials: string | TokenCredentials,
+        public readonly authCredentials: string,
         public readonly host: string='v2.convertapi.com'
     ) {}
 
@@ -45,14 +40,19 @@ export default class ConvertApi {
     public convert(fromFormat: string, toFormat: string, params: IParams): Promise<Result> {
         return Promise.resolve(params.dto)
             .then(dto => {
-                let altConvParam = dto.Parameters.filter(p => p.Name.toLowerCase() == 'converter')
-                let converterPath = altConvParam?.length > 0 ? `/converter/${altConvParam[0].Value}` : ''                    
-                
-                let auth = typeof this.credentials === 'string' ? `secret=${this.credentials}` : `apikey=${this.credentials.apiKey}&token=${this.credentials.token}`
-                return fetch(`https://${this.host}/convert/${fromFormat}/to/${toFormat}${converterPath}?${auth}&storefile=true`, { method: 'POST', headers: { 'content-type': 'application/json' }, body: JSON.stringify(dto) })
-                    .then(r => Promise.all([r.ok, r.json()]))
-                    .then(([ok, o]) => ok ? o : Promise.reject<IResultErrorDto>(o))
-                    .then(dto => new Result(dto))
+                return fetch(`https://${this.host}/convert/${fromFormat}/to/${toFormat}?storefile=true`,
+                {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                            Authorization: `Bearer ${this.authCredentials}`
+                        },
+                        body: JSON.stringify(dto)
+                    }
+                )
+                .then(r => Promise.all([r.ok, r.json()]))
+                .then(([ok, o]) => ok ? o : Promise.reject<IResultErrorDto>(o))
+                .then(dto => new Result(dto))
             })
     }
 }
